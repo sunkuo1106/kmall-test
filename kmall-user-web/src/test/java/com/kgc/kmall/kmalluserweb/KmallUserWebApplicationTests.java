8b44371064056f075a5e4c6bec662a8517cdf8c4
sunkuo1106 <sunkuo1106@163.com>
2020-12-15 20:03:59 +0800

    完成

---
+++ kmall-user-web/src/test/java/com/kgc/kmall/kmalluserweb/KmallUserWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.kmalluserweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmallUserWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
