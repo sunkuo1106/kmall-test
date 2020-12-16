c5aa6c6539084da1549f671da84e39625e8d291a
sunkuo1106 <sunkuo1106@163.com>
2020-12-16 20:18:20 +0800

    完成1216

---
+++ kmall-api/src/main/java/com/kgc/kmall/service/CatalogService.java
@@ -0,0 +1,15 @@
+package com.kgc.kmall.service;
+
+import com.kgc.kmall.bean.PmsBaseCatalog1;
+import com.kgc.kmall.bean.PmsBaseCatalog2;
+import com.kgc.kmall.bean.PmsBaseCatalog3;
+
+import java.util.List;
+
+public interface CatalogService {
+    public List<PmsBaseCatalog1> getCatalog1();
+
+    List<PmsBaseCatalog2> getCatalog2(Integer catalog1Id);
+
+    List<PmsBaseCatalog3> getCatalog3(Long catalog2Id);
+}
